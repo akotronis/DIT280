3e0fcd98eedd55260159e4f2c57970d9e44014a0
akotronis <akotronis@wings-ict-solutions.eu>
2024-05-28 14:50:53 +0000

    New attempt

--- Assignment-2/cnf.hs
+++ Assignment-2/cnf.hs
@@ -4,27 +4,31 @@ data LogicExpr = Var String
                | Not LogicExpr
                deriving Show
 
--- cnf :: LogicExpr -> LogicExpr
--- cnf expr = cnfHelper (pushNegation expr) []
-
 pushNegation :: LogicExpr -> LogicExpr
 pushNegation (Var name) = Var name
 pushNegation (Not (Var name)) = Not (Var name)
-pushNegation (Not (Not inner)) = inner
+pushNegation (Not (Not inner)) = pushNegation inner
 pushNegation (Not (Or left right)) = And (pushNegation (Not left)) (pushNegation (Not right))
 pushNegation (Not (And left right)) = Or (pushNegation (Not left)) (pushNegation (Not right))
 pushNegation (Or left right) = Or (pushNegation left) (pushNegation right)
 pushNegation (And left right) = And (pushNegation left) (pushNegation right)
 
--- cnfHelper :: LogicExpr -> [LogicExpr] -> LogicExpr
--- cnfHelper (Var name) acc = And (acc ++ [Var name])
--- cnfHelper (Not expr) acc = cnfHelper (pushNegation expr) (acc ++ [Not (head acc)]) -- Apply De Morgan's after pushing negation inwards 
--- cnfHelper (Or left right) acc = let (leftCnf, leftAcc) = cnfHelper left []
---                                 (rightCnf, rightAcc) = cnfHelper right leftAcc
---                             in And (acc ++ leftAcc ++ rightAcc ++ [Or leftCnf rightCnf]) -- Apply distributive law
--- cnfHelper (And left right) acc = let (leftCnf, leftAcc) = cnfHelper left acc
---                                 (rightCnf, rightAcc) = cnfHelper right leftAcc
---                             in And (leftAcc ++ rightAcc ++ [And leftCnf rightCnf]) -- Apply distributive law
+cnfHelper :: LogicExpr -> LogicExpr
+cnfHelper (Var name) = Var name
+cnfHelper (Not expr) = Not (cnfHelper expr)
+cnfHelper (Or left right) = distribute (cnfHelper left) (cnfHelper right)
+cnfHelper (And left right) = And (cnfHelper left) (cnfHelper right)
+
+distribute :: LogicExpr -> LogicExpr -> LogicExpr
+distribute (And a b) y = And (distribute a y) (distribute b y)
+distribute x (And a b) = And (distribute x a) (distribute x b)
+distribute x y = Or x y
+
+cnf :: LogicExpr -> LogicExpr
+cnf expr = cnfHelper (pushNegation expr)
+
+
+
 
 -- Tests
 main :: IO ()
